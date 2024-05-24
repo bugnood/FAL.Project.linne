413fbc7543ba5137b1711ed139d5404eaacb3676
daisuke.abe <commityneo@gmail.com>
2024-05-25 08:52:00 +0900

    #5 create index.ts

---
+++ server/index.ts
@@ -0,0 +1,18 @@
+// server/index.ts
+import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express サーバーが実行中です。');
+});
+
+app.listen(PORT, () => {
+  console.log(`サーバーがポート ${PORT} で実行中です。`);
+});
