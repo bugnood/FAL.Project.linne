3430c1b0d00db6f3c810a2bf3040db13cf333ae7
daisuke.abe <commityneo@gmail.com>
2024-06-02 16:17:24 +0900

    header footer change css

--- client/src/layout/Header.tsx
+++ client/src/layout/Header.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import '../style/app.css'; 
+import '../style/app.css';
+import appLogo from '../assets/linne.svg';
 
 const Header: React.FC = () => {
   return (
     <header>
-      <h1>My App</h1>
+      <div className='header-logo-content'>
+        <img src={appLogo} />
+        <h1 className='header-logo'>Linne</h1>
+      </div>
     </header>
   );
 };
