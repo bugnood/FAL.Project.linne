b957d3ca0b69f04376e78fbc8024f3e54f6185f3
daisuke.abe <commityneo@gmail.com>
2024-06-02 10:23:08 +0900

    home_content_change_css

--- client/src/layout/Sidebar.tsx
+++ client/src/layout/Sidebar.tsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../style/app.css'; 
+import '../style/app.css';
 
 const Sidebar: React.FC = () => {
   return (
     <aside>
-      <nav>
+      <div className='sidebar-panel'>
+        <div className='sidebar-panel-heading'>メニュー</div>
+        <div className='sidebar-list-group'>
+          <Link to="/home">タイムライン</Link>
+          <Link to="/create-post">新規投稿</Link>
+          <Link to="/register">新規登録</Link>
+          <Link to="/">ログイン画面</Link>
+        </div>
+      </div>
+      {/* <nav>
         <ul>
           <li><Link to="/home">タイムライン</Link></li>
           <li><Link to="/create-post">新規投稿</Link></li>
           <li><Link to="/register">新規登録</Link></li>
           <li><Link to="/">ログイン画面</Link></li>
         </ul>
-      </nav>
+      </nav> */}
     </aside>
   );
 };
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,13 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 import { FaHeart, FaStar } from 'react-icons/fa';
 import '../style/home.css';
 import miraiSpectrumIcon from '../assets/mirai-spectrum_icon.png';
 
 const Home: React.FC = () => {
   const [posts, setPosts] = useState<any[]>([]);
-  const [editMode, setEditMode] = useState<number | null>(null);
-  const [editedContent, setEditedContent] = useState<string>('');
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -26,51 +23,6 @@ const Home: React.FC = () => {
 
     fetchPosts();
   }, []);
-
-  const handleEdit = (index: number) => {
-    setEditMode(index);
-    setEditedContent(posts[index].content);
-  };
-
-  const handleSave = async (index: number) => {
-    try {
-      const response = await fetch('http://localhost:5002/api/posts', {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          id: posts[index].post_id,
-          content: editedContent,
-        }),
-      });
-      if (!response.ok) {
-        throw new Error('Failed to save post');
-      }
-      setPosts(prevPosts => {
-        const newPosts = [...prevPosts];
-        newPosts[index].content = editedContent;
-        return newPosts;
-      });
-      setEditMode(null);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const handleDelete = async (index: number) => {
-    try {
-      const response = await fetch(`http://localhost:5002/api/posts/${posts[index].post_id}`, {
-        method: 'DELETE',
-      });
-      if (!response.ok) {
-        throw new Error('Failed to delete post');
-      }
-      setPosts(prevPosts => prevPosts.filter((_, i) => i !== index));
-    } catch (error) {
-      console.error(error);
-    }
-  };
 
   const handleLike = async (index: number) => {
     const postId = posts[index].post_id;
@@ -114,12 +66,6 @@ const Home: React.FC = () => {
 
   return (
     <div className="home-container">
-      {/* <h2>ホーム画面</h2>
-      <p>楽しく日常を呟こう！</p>
-      <Link to="/create-post">
-        <button className="create-post-button">新規投稿</button>
-      </Link>
-      <h3>投稿一覧</h3> */}
       <div className="home-post">
         {posts.map((post, index) => (
           <div key={post.post_id} className='post-content-area'>
@@ -145,38 +91,6 @@ const Home: React.FC = () => {
             </div>
           </div>
         ))}
-
-        {/* {posts.map((post, index) => (
-          <div key={post.post_id} className="post-card">
-            {editMode === index ? (
-              <div className="edit-container">
-                <input
-                  type="text"
-                  value={editedContent}
-                  onChange={(e) => setEditedContent(e.target.value)}
-                  className="edit-input"
-                />
-                <button onClick={() => handleSave(index)} className="save-button">Save</button>
-              </div>
-            ) : (
-              <div className="post-content">
-                <p>{post.content}</p>
-                <div className="button-container">
-                  <button onClick={() => handleEdit(index)} className="edit-button">Edit</button>
-                  <button onClick={() => handleDelete(index)} className="delete-button">Delete</button>
-                  <button onClick={() => handleLike(index)} className="like-button">
-                    <FaHeart color="red" />
-                    <span>{post.likes_count}</span>
-                  </button>
-                  <button onClick={() => handleFavorite(index)} className="favorite-button">
-                    <FaStar color="yellow" />
-                    <span>{post.favorite_count}</span>
-                  </button>
-                </div>
-              </div>
-            )}
-          </div>
-        ))} */}
       </div>
     </div>
   );
