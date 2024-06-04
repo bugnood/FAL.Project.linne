de33d5b7f35dd88e69331a4775b209de49da9dec
daisuke.abe <commityneo@gmail.com>
2024-06-04 21:16:02 +0900

    timeline css change

--- client/src/pages/Timeline.tsx
+++ client/src/pages/Timeline.tsx
@@ -1,10 +1,31 @@
 import React, { useEffect, useState } from 'react';
-import { FaHeart, FaStar } from 'react-icons/fa';
+import { FaStar } from 'react-icons/fa';
+import { CiCirclePlus } from "react-icons/ci";
+import { IoIosCloseCircle } from "react-icons/io";
+import ReactModal from 'react-modal';
 import '../style/timeline.css';
 import miraiSpectrumIcon from '../assets/mirai-spectrum_icon.png';
 
 const Timeline: React.FC = () => {
+
+    // タイムライン表示
     const [posts, setPosts] = useState<any[]>([]);
+
+    // 投稿内容の状態を管理
+    const [newPopsContents, setNewPopsContents] = useState<string>('');
+
+    // モーダルの状態を管理
+    const [popsModalIsOpen, setPopsModalIsOpen] = useState(false);
+
+    // ログインモーダルを開く関数
+    const openPopsModal = () => {
+        setPopsModalIsOpen(true);
+    };
+
+    // 登録モーダルを閉じる関数
+    const closePopsModal = () => {
+        setPopsModalIsOpen(false);
+    };
 
     useEffect(() => {
         const fetchPosts = async () => {
@@ -23,26 +44,6 @@ const Timeline: React.FC = () => {
 
         fetchPosts();
     }, []);
-
-    // const handleLike = async (index: number) => {
-    //     const post_no = posts[index].post_no;
-    //     try {
-    //         const response = await fetch(`http://localhost:5002/api/posts/like/${post_no}`, {
-    //             method: 'PUT',
-    //         });
-    //         if (!response.ok) {
-    //             throw new Error('Failed to update like count');
-    //         }
-    //         const data = await response.json();
-    //         setPosts((prevPosts) => {
-    //             const newPosts = [...prevPosts];
-    //             newPosts[index].likes_count = data.likes_count;
-    //             return newPosts;
-    //         });
-    //     } catch (error) {
-    //         console.error(error);
-    //     }
-    // };
 
     const handleFavorite = async (index: number) => {
         const post_no = posts[index].post_no;
@@ -79,10 +80,6 @@ const Timeline: React.FC = () => {
                             </div>
                             <p className='post-content'>{post.content}</p>
                             <div className='reaction-button'>
-                                {/* <button onClick={() => handleLike(index)} className="like-button">
-                                    <FaHeart color="#ddd" />
-                                    <span>{post.likes_count}</span>
-                                </button> */}
                                 <button onClick={() => handleFavorite(index)} className="favorite-button">
                                     <FaStar color="#ddd" />
                                     <span>{post.favorites_count}</span>
@@ -91,6 +88,27 @@ const Timeline: React.FC = () => {
                         </div>
                     </div>
                 ))}
+                <button className='timeline-new-pops-button' onClick={openPopsModal}>
+                    <CiCirclePlus className='timeline-new-pops-icon' color='#009da3' size={80} />
+                </button>
+                <ReactModal
+                    isOpen={popsModalIsOpen}
+                    onRequestClose={closePopsModal}
+                    shouldCloseOnOverlayClick={false}
+                    contentLabel="新規投稿モーダル"
+                    className={"timeline-new-pops-modal"}
+                    overlayClassName="custom-overlay"
+                >
+                    <div className='timeline-modal-top-area'>
+                        <button className='timeline-modal-close-button' onClick={closePopsModal}><IoIosCloseCircle size={35} /></button>
+                    </div>
+                    <textarea
+                        placeholder='今、何してる？'
+                        maxLength={140}
+                        value={newPopsContents}
+                        onChange={(e) => setNewPopsContents(e.target.value)}
+                        className="timeline-new-pops-contents" />
+                </ReactModal>
             </div>
         </div>
     );
