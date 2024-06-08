8e84329dfc788073b0217cf1af66e661b1cc5995
daisuke.abe <commityneo@gmail.com>
2024-06-08 11:10:08 +0900

    layout change

--- client/src/layout/Layout.tsx
+++ client/src/layout/Layout.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import Footer from './Footer';
-import '../style/app.css'; 
+import '../style/app.css';
 
 interface LayoutProps {
   children: React.ReactNode;
